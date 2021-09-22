02e2ff0442345a08c1b7039fc5866b2e96b979b4
MrSnailman <73664326+MrSnailman@users.noreply.github.com>
2021-09-22 08:21:06 -0400

    Delete nav.php

--- nav.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo 
-'<ul>
-  <li><a class="active" href="page1.html">Home</a></li>
-  <li><a href="page2.html">News</a></li>
-  <li><a href="page3.html">Contact</a></li>
-  <li><a href="page4.html">About</a></li>
-</ul>';
-?>
