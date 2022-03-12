9055505214e386b306711bab2af2c66bc9bc804f
MrSnailman <73664326+MrSnailman@users.noreply.github.com>
2022-03-12 11:41:56 -0500

    Update arc.js

--- arc.js
+++ arc.js
@@ -1,1 +1,1 @@
-<script async src="https://arc.io/widget.min.js#ad7ZgW5u" async src="https://arc.io/widget.min.js#c7kVikx6"></script>
+<script async src="https://arc.io/widget.min.js#ad7ZgW5u"></script>
