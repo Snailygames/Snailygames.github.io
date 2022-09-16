8426732c60efa80215464aecf736e2cee2f37fcf
MrSnailman <73664326+MrSnailman@users.noreply.github.com>
2022-09-16 09:20:46 -0400

    Update CoreGlueCode.js

--- gba/user_scripts/CoreGlueCode.js
+++ gba/user_scripts/CoreGlueCode.js
@@ -11,7 +11,7 @@
 var games = {
 	"GTAdvance2":"GTAdvance2",
 	"GTAdvance3":"GTAdvance3",
-	"CastlevaniaCOM":"castlevaniacom",
+	"castlevaniacom":"CastlevaniacCOM",
     "advancewars":"Advance Wars",
     "advancewars2":"Advance Wars 2",
     "aladdin":"Aladdin",
