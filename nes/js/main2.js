eef97229b5f7b43667c6819e4490f5eb670966da
MrSnailman <73664326+MrSnailman@users.noreply.github.com>
2022-09-16 12:47:13 -0400

    Update main2.js

--- nes/js/main2.js
+++ nes/js/main2.js
@@ -1,116 +1,78 @@
-<!DOCTYPE html>
-<html>
-<title>SnailyGames</title>
-<head>
-<script async src="https://arc.io/widget.min.js#ad7ZgW5u"></script>
-  <link rel="shortcut icon" type="image/x-icon" href="favivon.ico">
-</head>
-<style>
-body {
-  margin: 0;
-  font-family: Arial, Helvetica, sans-serif;
+// create a nes object
+let nes = new Nes();
+
+// load a rom (rom as an Uint8Array)
+if(nes.loadRom(rom)) {
+  // after loading, do a hard reset
+  nes.reset(true);
+  // rom is now loaded
+} else {
+  // rom load failed
 }
 
-.topnav {
-  overflow: hidden;
-  background-color: #333;
+// run a frame (should be called 60 times per second)
+nes.runFrame();
+
+// get the image output
+// data should be an Uint8Array with 256*240*4 values, 4 for each pixel (r, g, b, a),
+// as in the data for a canvas-2d-context imageData object
+nes.getPixels(data);
+// for example:
+// once
+let ctx = canvas.getContext("2d");
+let imgData = ctx.createImageData(256, 240);
+// every frame
+nes.getPixels(imgData.data);
+ctx.putImageData(imgData, 0, 0);
+
+// get the sound output
+// audioData should be an Float64Array, and will be filled with the amount of samples specified
+// (usually the sample rate divided by 60)
+nes.getSamples(audioData, 44100 / 60);
+// see js/audio.js for a example on how this can be played.
+// the basic idea is to use an ScriptProcessorNode, fill a buffer (audioData above)
+// with the samples and write it to a ring-buffer each frame, and have the ScriptProcessorNode's
+// callback read from the ring-buffer, making sure that the read-position is always behind the write-position
+
+// set controller state
+nes.setButtonPressed(1, nes.INPUT.B); // player 1 is now pressing the B button
+nes.setButtonPressed(2, nes.INPUT.SELECT); // player 2 is now pressing the select button
+nes.setButtonReleased(1, nes.INPUT.B); // player 1 released B
+nes.setButtonReleased(2, nes.INPUT.SELECT); // now no buttons are pressed anymore
+// nes.INPUT contains A, B, SELECT, START, UP, DOWN, LEFT and RIGHT
+
+
+// other functions (only call if a rom is loaded)
+
+nes.reset(); // soft reset (as in the reset button being pressed, ram is retained)
+nes.reset(true); // hard reset (as in the NES being turned off and on again, ram is reset)
+let state = nes.getState(); // get the full state as an object
+if(nes.setState(state)) {
+  // the state-object has been loaded
+} else {
+  // failed to load the state-object
+}
+let battery = nes.getBattery(); // get the battery-backed ram, or undefined if the loaded rom does not have battery-backed ram
+if(nes.setBattery(battery)) {
+  // loaded battery data
+  // (trying to load battery for a rom without battery-backed ram is also deemed successful)
+} else {
+  // failed to load battery data
 }
 
-.topnav a {
-  float: left;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
+nes.cycle(); // run a single PPU cycle (and a CPU cycle every three calls)
+nes.peak(0x8000); // peaks a value from the CPU address-space (will have no side effects)
+nes.mapper.ppuPeak(0x1204); // peaks a value from the PPU address-space (will have no side effects)
+nes.read(0x2007); // reads a value from the CPU address-space (can have side effects)
+nes.mapper.ppuRead(0x2690); // reads a value from the PPU address-space (can have side effects)
+nes.write(0x0723, 0x12); // writes a value to the CPU address-space
+nes.mapper.ppuWrite(0x23c0, 0x34); // Writes a value to the PPU address-space
+// note that reading from PPU address-space does not allow reading the palette from 0x3f00-0x3fff
+// it will return the nametable-data 'behind' it
 
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
+// callbacks, these will be called during execution of nes.cycle() or nes.runFrame() when they are assigned a function
+nes.onread = (address, value) => {console.log(`read ${value} from ${address}`)};
+nes.onwrite = (address, value) => {console.log(`wrote ${value} to ${address}`)};
+nes.onexecute = (address, value) => {console.log(`executed from ${address} (opcode byte: ${value})`)};
 
-.topnav a.active {
-  background-color: #4CAF50;
-  color: white;
-}
-/* The side navigation menu */
-.sidebar {
-  margin: 0;
-  padding: 0;
-  width: 200px;
-  background-color: #333;
-  position: absolute;
-  height: 100%;
-  overflow: auto;
-}
-
-/* Sidebar links */
-.sidebar a {
-  display: block;
-  color: white;
-  padding: 16px;
-  text-decoration: none;
-}
-
-/* Active/current link */
-.sidebar a.active {
-  background-color: #4CAF50;
-  color: white;
-}
-
-/* Links on mouse-over */
-.sidebar a:hover:not(.active) {
-  background-color: #555;
-  color: white;
-}
-
-/* Page content. The value of the margin-left property should match the value of the sidebar's width property */
-div.content {
-  margin-left: 200px;
-  padding: 1px 16px;
-  height: 670px;
-}
-
-/* On screens that are less than 700px wide, make the sidebar into a topbar */
-@media screen and (max-width: 700px) {
-  .sidebar {
-    width: 100%;
-    height: auto;
-    position: relative;
-  }
-  .sidebar a {float: left;}
-  div.content {margin-left: 0;}
-}
-
-/* On screens that are less than 400px, display the bar vertically, instead of horizontally */
-@media screen and (max-width: 400px) {
-  .sidebar a {
-    text-align: center;
-    float: none;
-  }
-}
-</style>
-
-
-<!-- The sidebar -->
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
-<body ng-app="">
-
-<div ng-include="'../sidebar.html'"></div>
-
-<!-- Page content -->
-<div class="content">
-<body style="background-color:grey;">
-</div>
-  <div>
-    <div id="contentframe" style="position:absolute; top: 55px; left: 210px;">
-    <iframe id="ifrm" name="ifrm" src="../gba/launcher.html#castlevania"
-" scrolling="no" width="1167" height="649" align="center" frameborder="0">
-    </iframe>
-  </div>
-    <div>
-  
-    </div>
-</body>
-</html>
+// more functions and properties are available, just checking the .js files themselves is probably the easiest way to see these
