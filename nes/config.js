0509c35c6fa636848d810de9e14e93ff013a07a7
MrSnailman <73664326+MrSnailman@users.noreply.github.com>
2022-09-18 16:50:57 -0400

    Update config.js

--- nes/config.js
+++ nes/config.js
@@ -1,10 +1,6 @@
     const nes = cfxnes();
     const {fullscreen} = nes;
     var fps = document.getElementById('fps');
-
     cfxnes({video: {scale: 2.8}});
 
     
-    setInterval(function() {
-      fps.innerHTML = nes.running ? ('FPS: ' + ~~nes.fps) : '';
-    }, 500);
