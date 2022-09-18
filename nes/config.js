7b4200a3d353930da649bf40185cc029bb68bd49
MrSnailman <73664326+MrSnailman@users.noreply.github.com>
2022-09-18 16:59:28 -0400

    Update config.js

--- nes/config.js
+++ nes/config.js
@@ -2,4 +2,4 @@
     const {fullscreen} = nes;
     var fps = document.getElementById('fps');
     cfxnes({video: {scale: 2.8}});
-    fullscreen.type = 'normalized';
+    fullscreen.type = 'stretched';
