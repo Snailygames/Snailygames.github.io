95c2c88b6742e9c04f4ddd8737fc97c0ec4d0e18
MrSnailman <73664326+MrSnailman@users.noreply.github.com>
2022-09-20 10:15:25 -0400

    Update config.js

--- nes/config.js
+++ nes/config.js
@@ -3,3 +3,23 @@
     var fps = document.getElementById('fps');
     cfxnes({video: {scale: 2.8}});
     fullscreen.type = 'normalized';
+    
+function loadNVRAM() {
+  return localforage.getItem(nes.rom.sha1).then(data => {
+    if (nes.nvram && data) {
+      nes.nvram.set(data);
+    }
+  }).catch(error => {
+    console.error(error);
+  });
+}
+
+function saveNVRAM() {
+  if (nes.nvram) {
+    localforage.setItem(nes.rom.sha1, nes.nvram).catch(error => {
+      console.error(error);
+    });
+  }
+}
+
+loadNVRAM();
